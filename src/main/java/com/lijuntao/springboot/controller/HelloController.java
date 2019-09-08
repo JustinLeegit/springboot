1112bc9f96f7a68ed81a4e689d42db471e2ffcb4
lijt90 <627082796@qq.com>
2019-09-08 11:07:53 +0800

    udpate

--- src/main/java/com/lijuntao/springboot/controller/HelloController.java
+++ src/main/java/com/lijuntao/springboot/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping(value="/hello",method = RequestMethod.GET)
+
     public String say(){
         return "hello China";
     }
